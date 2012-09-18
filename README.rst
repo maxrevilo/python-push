987772b74a06686a41fc87c8eb4a76ccfad696ff
Oliver Perez <oliver.a.perez.c@gmail.com>
2012-09-18 13:42:06 -0430

    PyPi Setups

---
+++ README.rst
@@ -0,0 +1,23 @@
+===========
+Python Push Notification Manager
+===========
+
+Current State
+=============
+
+This library is on its very initial state.
+I expect to support the first two platforms for december.
+
+Platforms:
+==========
+At short term:
+
+1 Android GCM
+2 Blackberry Push Protocol.
+3 iOS APN
+
+At long term:
+4 Windows Phone 7
+5 Symbian
+6 Browsers (With sockets)
+7 Windows 8
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='python-push',
+    version='0.1.0',
+    author='Oliver Perez',
+    author_email='oliver.a.perez.c@gmail.com',
+    packages=['python-push'],
+    url='http://pypi.python.org/pypi/python-push/',
+    license='LICENSE.txt',
+    description='Python server-side library for sending Push Notifications to multiple mobile platforms.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "grequests",
+    ],
+)
